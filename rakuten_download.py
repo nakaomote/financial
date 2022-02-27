50d6c21175be5202ea89bf562dfb852b5c971afa
William Fletcher <uiriamu.furecchaa@gmail.com>
2022-02-27 11:04:37 +0000

    Rakuten download with password from config and date set to today

--- rakuten_download.py
+++ rakuten_download.py
@@ -5,17 +5,27 @@ CSV download
 """
 
 import requests
+from datetime import datetime
+from pytz import timezone
+import configparser
+import os
+
+config = configparser.ConfigParser()
+config.read(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'config.ini'))
+settings = config['rakuten']
 
 session = requests.Session()
 session.get("https://fes.rakuten-bank.co.jp/MS/main/RbS?CurrentPageID=START&&COMMAND=LOGIN")
+
+dt = datetime.now(timezone("Asia/Tokyo"))
 
 post = {
     "LOGIN_SUBMIT": "1",
     "jsf_sequence": "1",
     "LOGIN:_link_hidden_": "LOGIN:_idJsp43",
     "LOGIN:LOGIN_PASSWORD_CHECK": "TOOLTIP_CHECK",
-    "LOGIN:USER_ID": "YOURUSER",
-    "LOGIN:LOGIN_PASSWORD": "YOURPASS"
+    "LOGIN:USER_ID": settings["user"],
+    "LOGIN:LOGIN_PASSWORD": settings["pass"]
 }
 
 session.post("https://fes.rakuten-bank.co.jp/MS/main/fcs/rb/fes/jsp/mainservice/Security/LoginAuthentication/Login/Login.jsp", post)
@@ -24,7 +34,7 @@ session.get("https://fes.rakuten-bank.co.jp/MS/main/gns?COMMAND=CREDIT_DEBIT_INQ
 
 post = {
     "FORM_DOWNLOAD_IND:datepicker_from": '2021/11/23',
-    "FORM_DOWNLOAD_IND:datepicker_to": '2022/01/25',
+    "FORM_DOWNLOAD_IND:datepicker_to": dt.strftime("%Y/%m/%d"),
     "FORM_DOWNLOAD_IND:ACCOUNTOPEN_DATE": '2014/09/04',
     "FORM_DOWNLOAD_IND:DOWNLOAD_TYPE": '0',
     "FORM_DOWNLOAD_IND_SUBMIT": '1',
@@ -46,11 +56,11 @@ r = session.post("https://fes.rakuten-bank.co.jp/MS/main/fcs/rb/fes/jsp/mainserv
 
 post = {
         "FORM_DOWNLOAD:EXPECTED_DATE_FROM_YEAR": "2021",
-        "FORM_DOWNLOAD:EXPECTED_DATE_FROM_MONTH": "10",
-        "FORM_DOWNLOAD:EXPECTED_DATE_FROM_DAY": "27",
-        "FORM_DOWNLOAD:EXPECTED_DATE_TO_YEAR": "2022",
-        "FORM_DOWNLOAD:EXPECTED_DATE_TO_MONTH": "01",
-        "FORM_DOWNLOAD:EXPECTED_DATE_TO_DAY": "25",
+        "FORM_DOWNLOAD:EXPECTED_DATE_FROM_MONTH": "11",
+        "FORM_DOWNLOAD:EXPECTED_DATE_FROM_DAY": "23",
+        "FORM_DOWNLOAD:EXPECTED_DATE_TO_YEAR": dt.strftime("%Y"),
+        "FORM_DOWNLOAD:EXPECTED_DATE_TO_MONTH": dt.strftime("%m"),
+        "FORM_DOWNLOAD:EXPECTED_DATE_TO_DAY": dt.strftime("%d"),
         "FORM_DOWNLOAD:transitionSourceFlag": "",
         "FORM_DOWNLOAD_SUBMIT": "1",
         "FORM_DOWNLOAD:_link_hidden_": "FORM_DOWNLOAD:_idJsp1385"
