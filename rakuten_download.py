856e32990a9562540079949ab548f36efeed7544
William Fletcher <uiriamu.furecchaa@gmail.com>
2022-07-25 08:52:39 +0000

    Rakuten download needs selenium to just make it work

--- rakuten_download.py
+++ rakuten_download.py
@@ -4,68 +4,64 @@
 CSV download
 """
 
-import requests
-from datetime import datetime
+from datetime import datetime, timedelta
 from pytz import timezone
 import configparser
 import os
+import time
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 config = configparser.ConfigParser()
 config.read(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'config.ini'))
 settings = config['rakuten']
 
-session = requests.Session()
-session.get("https://fes.rakuten-bank.co.jp/MS/main/RbS?CurrentPageID=START&&COMMAND=LOGIN")
+options = Options()
+options.add_argument("--disable-extensions")
+options.add_argument("--disable-gpu")
+options.add_argument("disable-infobars")
+options.add_argument("start-maximized")
+options.add_argument('--disable-dev-shm-usage')
+# XXX TODO
+#options.add_argument('--headless')
+options.add_argument('--no-sandbox')
+options.add_argument("--remote-debugging-port=4444")
+options.add_experimental_option("prefs", {
+    "download.default_directory": "/home/william.fletcher/work/financial",
+    "download.prompt_for_download": False,
+    "download.directory_upgrade": True,
+    "safebrowsing.enabled": True,
+    "profile.default_content_setting_values.automatic_downloads": 1
+})
+driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+
+driver.get("https://fes.rakuten-bank.co.jp/MS/main/RbS?CurrentPageID=START&&COMMAND=LOGIN")
+
+driver.execute_script("arguments[0].value = '" + settings["user"] + "';", WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID, "LOGIN:USER_ID"))))
+driver.execute_script("arguments[0].value = '" + settings["pass"] + "';", WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID, "LOGIN:LOGIN_PASSWORD"))))
+driver.find_element(By.ID, "LOGIN:_idJsp43").click()
+WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID, "HEADER:j_id_v5"))).click()
 
 dt = datetime.now(timezone("Asia/Tokyo"))
+start_date = datetime.now() - timedelta(35)
+WebDriverWait(driver, 100).until(lambda driver: driver.execute_script('return document.readyState') == 'complete')
+driver.execute_script("arguments[0].value = '" + start_date.strftime("%Y/%m/%d") + "';", WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID, "FORM_DOWNLOAD_IND:datepicker_from"))))
+driver.execute_script("arguments[0].value = '" + dt.strftime("%Y/%m/%d") + "';", WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID, "FORM_DOWNLOAD_IND:datepicker_to"))))
 
-post = {
-    "LOGIN_SUBMIT": "1",
-    "jsf_sequence": "1",
-    "LOGIN:_link_hidden_": "LOGIN:_idJsp43",
-    "LOGIN:LOGIN_PASSWORD_CHECK": "TOOLTIP_CHECK",
-    "LOGIN:USER_ID": settings["user"],
-    "LOGIN:LOGIN_PASSWORD": settings["pass"]
-}
+driver.execute_script("validateInquiryTerm = function validateInquiryTerm(downloadType, accountStartDate, term) { if (downloadType === 1) { document.getElementsByClassName('CREDITDEBITINQUIRY_KOJIN_CSV_DOWNLAOD')[0].click(); } else if (downloadType === 2) { document.getElementsByClassName('CREDITDEBITINQUIRY_KOJIN_PDF_DOWNLAOD')[0].click(); } return; } ")
 
-session.post("https://fes.rakuten-bank.co.jp/MS/main/fcs/rb/fes/jsp/mainservice/Security/LoginAuthentication/Login/Login.jsp", post)
-session.get("https://fes.rakuten-bank.co.jp/MS/main/gns?COMMAND=BALANCE_INQUIRY_START&=&CurrentPageID=HEADER_FOOTER_LINK")
-session.get("https://fes.rakuten-bank.co.jp/MS/main/gns?COMMAND=CREDIT_DEBIT_INQUIRY_START&&CurrentPageID=HEADER_FOOTER_LINK")
+WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID, "FORM_DOWNLOAD_IND:j_id_68a"))).click()
 
-post = {
-    "FORM_DOWNLOAD_IND:datepicker_from": '2021/11/23',
-    "FORM_DOWNLOAD_IND:datepicker_to": dt.strftime("%Y/%m/%d"),
-    "FORM_DOWNLOAD_IND:ACCOUNTOPEN_DATE": '2014/09/04',
-    "FORM_DOWNLOAD_IND:DOWNLOAD_TYPE": '0',
-    "FORM_DOWNLOAD_IND_SUBMIT": '1',
-    "FORM_DOWNLOAD_IND:_link_hidden_": 'FORM_DOWNLOAD_IND:_idJsp695'
-}
+driver.find_element(By.LINK_TEXT, "デビット利用明細はこちら").click()
 
-r = session.post("https://fes.rakuten-bank.co.jp/MS/main/fcs/rb/fes/jsp/mainservice/Inquiry/CreditDebitInquiry/CreditDebitInquiry/CreditDebitInquiry.jsp", data=post)
-with open("rakuten_bank.csv", "w") as rb:
-    rb.write(r.text)
+driver.execute_script("arguments[0].value = '" + start_date.strftime("%m") + "';", WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID, "FORM_DOWNLOAD:EXPECTED_DATE_FROM_MONTH"))))
+driver.execute_script("arguments[0].value = '" + start_date.strftime("%d") + "';", WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID, "FORM_DOWNLOAD:EXPECTED_DATE_FROM_DAY"))))
 
-session.get("https://fes.rakuten-bank.co.jp/MS/main/gns?COMMAND=BALANCE_INQUIRY_START&=&CurrentPageID=HEADER_FOOTER_LINK")
-session.get("https://fes.rakuten-bank.co.jp/MS/main/gns?COMMAND=CREDIT_DEBIT_INQUIRY_START&&CurrentPageID=HEADER_FOOTER_LINK")
+WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID, "FORM_DOWNLOAD:_idJsp1385"))).click()
 
-post = {
-    "FORM_CONFIRM_SUBMIT": "1",
-    "FORM_CONFIRM:_link_hidden_": "FORM_CONFIRM:_idJsp194",
-}
-r = session.post("https://fes.rakuten-bank.co.jp/MS/main/fcs/rb/fes/jsp/mainservice/Inquiry/CreditDebitInquiry/CreditDebitInquiry/CreditDebitInquiry.jsp", data=post)
-
-post = {
-        "FORM_DOWNLOAD:EXPECTED_DATE_FROM_YEAR": "2021",
-        "FORM_DOWNLOAD:EXPECTED_DATE_FROM_MONTH": "11",
-        "FORM_DOWNLOAD:EXPECTED_DATE_FROM_DAY": "23",
-        "FORM_DOWNLOAD:EXPECTED_DATE_TO_YEAR": dt.strftime("%Y"),
-        "FORM_DOWNLOAD:EXPECTED_DATE_TO_MONTH": dt.strftime("%m"),
-        "FORM_DOWNLOAD:EXPECTED_DATE_TO_DAY": dt.strftime("%d"),
-        "FORM_DOWNLOAD:transitionSourceFlag": "",
-        "FORM_DOWNLOAD_SUBMIT": "1",
-        "FORM_DOWNLOAD:_link_hidden_": "FORM_DOWNLOAD:_idJsp1385"
-}
-
-r = session.post("https://fes.rakuten-bank.co.jp/MS/main/fcs/rb/fes/jsp/mainservice/CardManagement/DebitList/DebitList/DebitList.jsp", data=post)
-with open("rakuten_debit.csv", "w") as rb:
-    rb.write(r.text)
+time.sleep(10)
