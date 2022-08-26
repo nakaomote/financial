cf2bdba5948db94ab8903e844892630528cb0ee4
William Fletcher <uiriamu.furecchaa@gmail.com>
2022-08-26 17:23:35 +0000

    Paypay bank download mods to work

--- jnb_download.py
+++ jnb_download.py
@@ -12,7 +12,6 @@ from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support.ui import Select
-from selenium.webdriver import ActionChains
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 
@@ -34,25 +33,27 @@ options.add_experimental_option("prefs", {
     "download.default_directory": "/home/william.fletcher/work/financial",
     "download.prompt_for_download": False,
     "download.directory_upgrade": True,
-    "safebrowsing.enabled": True
+    "safebrowsing.enabled": True,
+    "profile.default_content_setting_values.automatic_downloads": 1
 })
 driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
 
 driver.get("https://login.paypay-bank.co.jp/wctx/1D1DFxFDg.do")
-driver.find_element_by_id("idTenNo").send_keys(settings["tenno"])
-driver.find_element_by_id("idKozaNo").send_keys(settings["kozano"])
-# XXX TODO: Send the password.
-# XXX kpcli --kdb ~/work/passwords --pwfile ~/crypt/master_password -c 'show banks/jnb -f' 
-driver.find_element_by_id("vBtn").click()
+
+driver.execute_script("arguments[0].value = '" + settings["tenno"] + "';", WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID, "idTenNo"))))
+driver.execute_script("arguments[0].value = '" + settings["kozano"] + "';", WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID, "idKozaNo"))))
+driver.execute_script("arguments[0].value = '" + settings["pass"] + "';", WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID, "idPw"))))
+
+driver.find_element(By.ID, "vBtn").click()
 time.sleep(12)
 
-driver.find_element_by_name("login").click()
-driver.find_element_by_css_selector("img[src='/commontpl/images/category/welcome_ic003.png']").click()
-Select(driver.find_element_by_name("ShokaiStartDateHiIn")).select_by_visible_text('22')
-driver.find_element_by_css_selector(".btn[value='照会']").click()
-driver.find_element_by_css_selector(".btn[value='CSV']").click()
-driver.find_element_by_class_name("notWinOpen").click()
-Select(driver.find_element_by_name("SyokaiStDate")).select_by_visible_text('22')
-driver.find_element_by_css_selector(".btn[value='照会']").click()
-driver.find_element_by_css_selector(".btn[value='CSV']").click()
+driver.find_element(By.NAME, "login").click()
+driver.find_element(By.CSS_SELECTOR, "img[src='/commontpl/images/category/welcome_ic003.png']").click()
+Select(driver.find_element(By.NAME, "ShokaiStartDateHiIn")).select_by_visible_text('22')
+driver.find_element(By.CSS_SELECTOR, ".btn[value='照会']").click()
+driver.find_element(By.CSS_SELECTOR, ".btn[value='CSV']").click()
+driver.find_element(By.CLASS_NAME, "notWinOpen").click()
+Select(driver.find_element(By.NAME, "SyokaiStDate")).select_by_visible_text('22')
+driver.find_element(By.CSS_SELECTOR, ".btn[value='照会']").click()
+driver.find_element(By.CSS_SELECTOR, ".btn[value='CSV']").click()
 time.sleep(10)
