ca9a53799542edb7d8a2b583242ad0c173e52d3b
William Fletcher <uiriamu.furecchaa@gmail.com>
2023-03-25 18:56:30 +0000

    Allow easier more consistent naming from Sony

--- descriptions/__init__.py
+++ descriptions/__init__.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import json
+from typing import Callable
 
 class Singleton(type):
     _instances = {}
@@ -12,21 +13,44 @@ class Singleton(type):
 
 class Descriptions(metaclass=Singleton):
 
+
     def __init__(self):
         with open("modifications.json", "r") as config_file:
             config = json.load(config_file)
             self.remove_trailing = config.pop("remove_trailing")
             self.cut_from = config.pop("cut_from")
+            self.substitutions = config.pop("substitutions")
 
-    def getName(self, name: str):
+        self.__functions = (
+            self.__removeTrailing,
+            self.__cutFrom,
+            self.__substitution,
+        )
+
+
+    def __removeTrailing(self, name: str) -> str:
+        remove_trailing: str
         for remove_trailing in self.remove_trailing:
             if name.startswith(remove_trailing):
-                name = remove_trailing
-                break
+                return remove_trailing
+        return name
 
+    def __cutFrom(self, name: str) -> str:
         for cut_from in self.cut_from:
             if cut_from in name:
-                name = name[0:name.index(cut_from)]
-                break
-
+                return name[0:name.index(cut_from)]
         return name
+
+    def __substitution(self, name: str) -> str:
+        for f, t in self.substitutions.items():
+            if f in name:
+                return name.replace(f,t)
+        return name
+
+    def __runFunctions(self, n: int, name: str) -> str:
+        if n - 1 <= 0:
+            return self.__functions[n-1](name)
+        return self.__runFunctions(n-1, self.__functions[n-1](name))
+
+    def getName(self, name: str):
+        return self.__runFunctions(n=len(self.__functions), name=name)
