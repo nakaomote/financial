5bab5506ec715204751baac5c85b10505b4950d1
William Fletcher <uiriamu.furecchaa@gmail.com>
2024-07-21 02:42:52 +0000

    improvements to paypay bank with a funny space change... lol

--- jnb_download.py
+++ jnb_download.py
@@ -41,15 +41,20 @@ driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), opti
 
 driver.get("https://login.paypay-bank.co.jp/wctx/1D1DFxFDg.do")
 
-driver.execute_script("arguments[0].value = '" + settings["tenno"] + "';", WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "idTenNo"))))
-driver.execute_script("arguments[0].value = '" + settings["kozano"] + "';", WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "idKozaNo"))))
-driver.execute_script("arguments[0].value = '" + settings["pass"] + "';", WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "idPw"))))
+while True:
+    try:
+        driver.execute_script("arguments[0].value = '" + settings["tenno"] + "';", WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.ID, "idTenNo"))))
+        driver.execute_script("arguments[0].value = '" + settings["kozano"] + "';", WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.ID, "idKozaNo"))))
+        driver.execute_script("arguments[0].value = '" + settings["pass"] + "';", WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.ID, "idPw"))))
 
-driver.find_element(By.ID, "vBtn").click()
-time.sleep(10)
+        driver.find_element(By.ID, "vBtn").click()
+        time.sleep(2)
 
-driver.find_element(By.NAME, "login").click()
-time.sleep(10)
+        driver.find_element(By.NAME, "login").click()
+        time.sleep(2)
+
+    except:
+        break
 
 driver.find_element(By.CSS_SELECTOR, "img[src='/commontpl/images/category/welcome_ic003.png']").click()
 WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.LINK_TEXT, "CSV"))).click()
@@ -57,7 +62,7 @@ WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.LINK_TEXT, "CSV")
 time.sleep(10)
 
 driver.switch_to.window(driver.window_handles[0])
-WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.LINK_TEXT, "Visaデビット　ご利用明細一覧"))).click()
+WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.LINK_TEXT, "Visaデビット ご利用明細一覧"))).click()
 time.sleep(10)
 
 WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.LINK_TEXT, "絞り込み"))).click()
@@ -69,4 +74,5 @@ time.sleep(10)
 WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".btn[value='照会']"))).click()
 WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.LINK_TEXT, "CSV"))).click()
 
-time.sleep(360)
+print("Press any key to continue ;)"
+wait_key()
