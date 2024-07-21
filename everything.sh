7b86b60e20d9f43f3eec7bf5b744e396fb5005e2
William Fletcher <uiriamu.furecchaa@gmail.com>
2024-07-21 05:27:03 +0000

    keep the standard everything.sh safe from running things after a clone

--- everything.sh
+++ everything.sh
@@ -7,9 +7,12 @@ set -o pipefail
 ## - Make transaction IDs link to date.
 ##   - This should avoid a lot of issues.
 ##   - Need to make sure if items change...
+#
 
+## TODO: automate this freaking login.
 # ANA suica visa credit card.
-#./vpass.py 202???.csv 74,692 6 | tee vpass.csv
+#mv -v ~/ダウンロード/202???.csv ./
+#./vpass.py 202???.csv 10,421 6 | tee vpass.csv
 
 # UFJ
 #./ufj_download.py
@@ -19,31 +22,29 @@ set -o pipefail
 #./seven_download.py
 #./seven.py *_debitmeisai.csv | tee seven.csv
 
-# Mobile pasumo
-# TODO: Add in a selenium portion that'll download the html.
-# TODO: Make the selenium ask about the capture.
-#./mobilePasumo.py | tee mobile.csv
-
 # PayPay bank
 #./jnb_download.py
-#./jnb.py NB*.csv > jnb.csv
+#./jnb.py 2024 6 1 NB*.csv | tee jnb.csv
 #./jnb.py shiftjis/* | tee jnb.csv
 
 # A's pasumo.
 #mv ~/ダウンロード/01010a107b19ba0e_* ./
 #./pasumo.py 01010a107b19ba0e_* > ~/work/financial/pasumo-a.csv
-
+#
 # Our Sony accounts.
-for user in $(awk -F '[_\\]]' ' /sony_/ { print $2 } ' config.ini); do
-    ./sony_download.py "${user}"
-    ./sony.py ./YenFutsuRireki.csv 2021-03-23 | tee "sony_${user}.csv"
-done
+#for user in $(awk -F '[_\\]]' ' /sony_/ { print $2 } ' config.ini); do
+#    ./sony_download.py "${user}"
+#    ./sony.py ./YenFutsuRireki.csv 2021-03-23 | tee "sony_${user}.csv"
+#done
 
 # Rakuten.
 #./rakuten_download.py
 #./rakuten.py RB-debitmeisai.csv RB-torihikimeisai.csv > rakuten.csv
 
 # Mobile pasumo
-# TODO: Add in a selenium portion that'll download the html.
-# TODO: Make the selenium ask about the capture.
+#./mobilePasumo.py
 #./mobilePasumo.py | tee mobile.csv
+#./mobilePasumo.py 02 | tee mobile.csv
+
+# Olive bank account.
+# ./olive.py meisai\ \(4\).csv | tee olive.csv
